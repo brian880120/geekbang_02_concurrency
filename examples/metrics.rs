1615ff13051431d07c3934fc97b4e99e6b6c462c
Bo Dai <bo.dai@aspireiq.com>
2024-07-11 22:14:14 -0400

    feature: init metrics

---
+++ examples/metrics.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+use concurrency::Metrics;
+use rand::Rng;
+use std::{thread, time::Duration};
+
+#[allow(unused)]
+const N: usize = 2;
+#[allow(unused)]
+const M: usize = 4;
+
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+    // for i in 0..100 {
+    //     metrics.inc("req.page.1");
+    //     metrics.inc("req.page.2");
+    //     if i % 2 == 0 {
+    //         metrics.inc("req.page.3");
+    //     }
+    // }
+
+    // for _ in 0..27 {
+    //     metrics.inc("call.thread.worker.1");
+    // }
+
+    println!("{:?}", metrics.snapshot());
+
+    Ok(())
+}
+
+#[allow(unused)]
+fn task_worker(idx: usize, mut metrics: Metrics) -> Result<()> {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+        metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
+    });
+    Ok(())
+}
+
+#[allow(unused)]
+fn process_requests(mut metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+        let page = rng.gen_range(1..256);
+        metrics.inc(format!("req.page.{}", page)).unwrap();
+    });
+}
