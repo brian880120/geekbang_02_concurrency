ae044c2de7e336039ff95901d4a50383f0b593bd
Bo Dai <bo.dai@aspireiq.com>
2024-07-17 16:16:03 -0400

    fix: not used unwrap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -29,20 +29,29 @@ fn main() -> Result<()> {
 
 #[allow(unused)]
 fn task_worker(idx: usize, mut metrics: Metrics) -> Result<()> {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
     Ok(())
 }
 
 #[allow(unused)]
-fn process_requests(mut metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(1..256);
-        metrics.inc(format!("req.page.{}", page)).unwrap();
+fn requests_worker(mut metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
